72426c05b085ce479a3e7eac9b711d42c63383df
Chriseze1233 <110237043+Chriseze1233@users.noreply.github.com>
2023-08-13 03:06:39 +0100

    Update click.js

--- click.js
+++ click.js
@@ -2,6 +2,7 @@ const frBtn = document.querySelector('#frward');
 const bktn = document.querySelector('#bkward');
 const body = document.querySelector('.h-hero');
 let count = 0;
+let intervalID;
 
 
 function changeBk(){
@@ -9,14 +10,26 @@ function changeBk(){
 }
 
 
+function addCount() {
+    if(intervalID){clearInterval(intervalID)}
+    intervalID =  setInterval(() => {
+        count++
+        if(count > srcs.length -1) {
+            count = 0;
+        }
+        changeBk()
+    }, 6000)
+}
+
+addCount()
+
 bktn.addEventListener('click', () => {
     count--;
-
  if(count < 0) {
     count = srcs.length -1;
  }
  changeBk();
-
+ addCount()
 })
 
 frBtn.addEventListener('click', () => {
@@ -25,5 +38,7 @@ frBtn.addEventListener('click', () => {
         count = 0;
     }
     changeBk();
+    addCount()
 })
+
 window.onload = changeBk();
